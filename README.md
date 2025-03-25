3e33eb40cfb4b79b28d188d824787ada2931610e
Eyyub Saral <eyyubsrl04@gmail.com>
2025-03-25 13:26:21 +0300

    Interview-08

---
+++ README.md
@@ -0,0 +1,5 @@
+<!--
+
+Bileşenin state'inde, başlangıçta örnek bir kullanıcı (örneğin, NamıkKorona1) ile doldurulmuş bir kullanıcı nesnelerini içeren bir dizi bulunmalıdır.
+
+Unutmayın ki özellikle hatalı durumları ve sınırlamaları doğru bir şekilde ele almanız ve kullanıcı deneyimini düşünerek hareket etmeniz önemlidir. Örnekte verilen resim ve açıklamaları temel alarak bileşeni oluşturmanız beklenmektedir. -->
---
+++ src/App.js
@@ -0,0 +1,94 @@
+import React, { useState } from "react";
+
+function App() {
+  return (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        paddingTop: 20,
+      }}
+    >
+      <ValidatedForm />
+    </div>
+  );
+}
+
+const ValidatedForm = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [accounts, setAccounts] = useState([
+    { username: "NamıkKorona1", password: "1234567" },
+  ]);
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    const account = accounts.find(
+      (acc) => acc.username === username && acc.password === password
+    );
+    if (!account) {
+      alert("Kullanıcı adınız ya da Şifreniz yanlış. Tekrar deneyiniz.");
+
+      if (password.length < 6) {
+        alert("Şifre kısa");
+      } else {
+        setPassword(""); // Şifre doğru uzunlukta olduğu için sıfırlanıyor.
+      }
+
+      setUsername(""); // Kullanıcı adı da sıfırlanıyor.
+    } else {
+      alert(`Hoş geldiniz, ${username}!`);
+      setPassword("");
+      setUsername("");
+    }
+  };
+
+  return (
+    <form
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        border: "solid",
+        padding: 10,
+      }}
+      onSubmit={onSubmit}
+    >
+      <h3>Login</h3>
+      <input
+        value={username}
+        type="text"
+        placeholder="Username"
+        onChange={(e) => {
+          if (e.target.value.length <= 20) {
+            setUsername(e.target.value);
+          } else {
+            alert("İsim 20 karakterden daha uzun olamaz!");
+          }
+        }}
+        className="border border-black my-[5px]"
+      />
+      <input
+        value={password}
+        type="password"
+        placeholder="Password"
+        onChange={(e) => {
+          if (e.target.value.length <= 20) {
+            setPassword(e.target.value);
+          } else {
+            alert("Şifre 20 karakterden daha uzun olamaz!");
+          }
+        }}
+        className="border border-black my-[10px]"
+      />
+      <button
+        style={{ alignSelf: "center" }}
+        onClick={onSubmit}
+        className="bg-gray-200 border border-black py-[1px] px-[5px] font-medium font-san"
+      >
+        Submit
+      </button>
+    </form>
+  );
+};
+
+export default App;
